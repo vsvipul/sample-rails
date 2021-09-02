e3df03bbe9c66a49a4039031b2c4b216c5d5999c
aparna-ravindra <aparna-ravindra@github.com>
2021-09-02 15:02:43 +0530

    test cases

--- test/controllers/articles_controller_test.rb
+++ test/controllers/articles_controller_test.rb
@@ -1,7 +1,8 @@
 require "test_helper"
 
 class ArticlesControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test "the truth" do
+    assert true
+  end
+
 end
